c67635e6a6e65a9832cc20cd21eed4d6b8120144
Peter Molnar <peter@petermolnar.hu>
2017-02-11 17:36:25 +0100

    ViewControllelr is EMPTY

--- TestForMultiple/ViewController.swift
+++ TestForMultiple/ViewController.swift
@@ -10,14 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
 
 }
 
