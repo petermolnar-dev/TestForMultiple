92d1e5369517d303044d515eae5dd71610630b39
Peter Molnar <peter@petermolnar.hu>
2017-02-11 16:37:01 +0100

    Initial Commit

---
+++ TestForMultiple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestForMultiple
+//
+//  Created by Peter Molnar on 11/02/2017.
+//  Copyright © 2017 Peter Molnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
