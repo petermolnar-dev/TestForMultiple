11e6499019da9f424c3e31a82cf914b4f9d6cde5
Peter Molnar <peter@petermolnar.hu>
2017-02-12 17:34:34 +0100

    Now I have a double backup remote.

--- TestForMultiple/AppDelegate.swift
+++ TestForMultiple/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // This is appdelegate
+        // Now I have a backup remote with duoble push
         
         return true
     }
